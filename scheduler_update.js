daafa7349a3e32f9581455c4a59a2d9183590dea
Trang Luong <luongtrang014@gmail.com>
2022-05-15 02:45:46 +0800

    update

--- scheduler_update.js
+++ scheduler_update.js
@@ -8,8 +8,8 @@ const {
 } = require("./controllers/googleapi.controller");
 
 const { parseDynamic } = require("./helpers/utils");
-const Web = require("./models/Web");
-// const { db } = require("./models/Web");
+const Website = require("./models/Website");
+
 const mongoURI = process.env.MONGO_DEV_URI;
 const db = mongoose.connection;
 
@@ -19,33 +19,33 @@ mongoose
   .connect(mongoURI)
   .then(() => {
     console.log(`DB connected`);
-    Web.find({}, (err, webs) => {
+    Website.find({}, (err, websites) => {
       if (err) return console.log(err);
-      webs.forEach(async (web) => {
-        let dbUpdate = await getSheetLastUpdate(web.spreadsheetId);
+      websites.forEach(async (website) => {
+        let dbUpdate = await getSheetLastUpdate(website.spreadsheetId);
         console.log("db", dbUpdate);
-        web.dbLastUpdate = Date.parse(dbUpdate.modifiedTime);
-        console.log("web", web.dbLastUpdate, web.lastUpdate);
+        website.dbLastUpdate = Date.parse(dbUpdate.modifiedTime);
+        console.log("web", website.dbLastUpdate, website.lastUpdate);
 
-        if (web.dbLastUpdate > web.lastUpdate) {
+        if (website.dbLastUpdate > website.lastUpdate) {
           // await db
           //   .collection("items")
-          //   .deleteMany({ spreadsheetId: web.spreadsheetId });
-          let data = await readData(web.spreadsheetId, web.range);
+          //   .deleteMany({ spreadsheetId: website.spreadsheetId });
+          let data = await readData(website.spreadsheetId, website.range);
           let header = data[0];
           data = data.slice(1);
           const promises = data.map(async (e, index) => {
             const obj = {};
             for (let i = 0; i < e.length; i++) {
-              obj.author = web.author;
-              obj.spreadsheetId = web.spreadsheetId;
+              obj.author = website.author;
+              obj.spreadsheetId = website.spreadsheetId;
               obj[header[i].toLowerCase()] = parseDynamic(e[i]);
             }
             data[index] = obj;
             let item = await db.collection("items").findOneAndUpdate(
               {
                 $and: [
-                  { spreadsheetId: web.spreadsheetId },
+                  { spreadsheetId: website.spreadsheetId },
                   { rowIndex: index },
                 ],
               },
@@ -57,7 +57,7 @@ mongoose
           await Promise.all(promises);
           const count = await db
             .collection("item")
-            .countDocuments({ spreadsheetId: web.spreadsheetId });
+            .countDocuments({ spreadsheetId: website.spreadsheetId });
           if (count > data.length) {
             await db
               .collection("item")
@@ -70,12 +70,12 @@ mongoose
 
           const itemList = await db
             .collection("items")
-            .find({ spreadsheetId: web.spreadsheetId })
+            .find({ spreadsheetId: website.spreadsheetId })
             .toArray();
-          web.data = itemList.map((item) => item._id);
+          website.data = itemList.map((item) => item._id);
 
-          web.lastUpdate = Date.now();
-          await web.save();
+          website.lastUpdate = Date.now();
+          await website.save();
           console.log("updated DB");
         } else {
           console.log("dont need update");
